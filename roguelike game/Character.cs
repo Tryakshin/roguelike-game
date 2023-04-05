b2fb474f4ec1ed2c0ed14f1299ce3aad9f1c14e3
KatantDev <vlad26@mail.ru>
2023-04-05 13:54:16 +1000

    Constructor of Character changed to protected

--- roguelike game/Character.cs
+++ roguelike game/Character.cs
@@ -5,7 +5,7 @@ public class Character : Entity
     protected int Health;
     protected int Damage;
 
-    public Character(int x, int y, int health, int damage, char symbol = 'M', 
+    protected Character(int x, int y, int health, int damage, char symbol = 'M', 
         ConsoleColor color = ConsoleColor.Magenta, bool collision = true)
         : base(x, y, collision, symbol, color)
     {
